50cf7f026be700069d4a48ead4269b868be6f8e6
HellRaiderz123 <57700863+HellRaiderz123@users.noreply.github.com>
2020-06-17 13:34:39 +0530

    Commit by tarun on 17th June 2020

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ $(function(){
     // accessing API_URL present in env.js
     console.log(API_URL);
     
-    $('#dynamic-content').text("Dynamic content");
+    //$('#dynamic-content').text("Dynamic content");
 
     
     
 
     // jQuery methods go here...
+    $('#dynamic-content').append('Enter First Number : <input type="text" id="firstNum"> <br><br><br> Enter Second Number : <input type="text" id="secondNum"><br><br>'); 
+    $('#dynamic-content').append('Result : <input type="text"  id="result"><br><br>');
+
+    $('#dynamic-content').append('<input type="button" id="btnAdd" value="Display Result">');
+
+    $('#btnAdd').on('click', function(){
+        let sum = parseInt ($('#firstNum').val()) + parseInt ($('#secondNum').val());
+        $('#result').val(sum);
+    });
   
 });
